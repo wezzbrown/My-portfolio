5be89a84535d30cd516ef6dbfc9493381995b37a
wezzbrown <sundaywisdomifiok@gmail.com>
2024-05-09 22:10:13 +0100

    portfolio

--- src/component/profile.jsx
+++ src/component/profile.jsx
@@ -9,8 +9,8 @@ const Profile = () => {
   return (
     <section id="profile" className="profile">
       <h2>About Me</h2>
-      <p>My name is Wisdom Sunday. I am a tech lover from time which lead me into learning  the fundermentals like Microsoft-word,Execl,Powerpoint and Corel-draw but yet it was still not enough for me to take tech as a side hustle, so i decided to advance into tech. which lead me into web development</p>
-       <p>I'm a passionate developer with experience in building modern web applications. I specialize in React and other langanges like html,bootstrap,javascript,tailwind,and of cause css because what is a website without css, which makes me a  front-end developer.</p>
+      <p>My name is Wisdom Sunday. I am a tech lover from time which lead me into learning  the fundamentals like Microsoft-word,Execl,Powerpoint and Corel-draw but yet it was still not enough for me to take tech as a side hustle, so I decided to advance into tech. which lead me into web development</p>
+       <p>I'm a passionate developer with experience in building modern web applications. I specialize in "React" and other langanges like html,bootstrap,javascript,tailwind,and of cause css because what is a website without css, which makes me a  front-end developer.</p>
       <div className="image">
       <img src={Image} alt="Profile" className="profile-pic" />
       <img src={Image2} alt="Profile" className="profile-pic" />
--- src/component/projects.jsx
+++ src/component/projects.jsx
@@ -1,38 +1,45 @@
-import React from 'react';
+import React from "react";
 // import './App.css';
-import Image6 from '../assets/image/image6.png'
-
+import Image6 from "../assets/image/image6.png";
 
 const Projects = () => {
   const projects = [
     {
-      name: 'Project 1',
-      description: 'This ia a project i did with bootstrap during my early days in web development.',
-      link: 'https://web-development-group2.vercel.app/',
+      name: "Project 1",
+      description:
+        "This ia a project i did with bootstrap during my early days in web development.",
+      link: "https://web-development-group2.vercel.app/",
     },
     {
-      name: 'Project 2',
-      description: 'This ia a project i did with html/css during my early days in web development..',
-      link: 'https://sidebar-transitioned.vercel.app/',
+      name: "Project 2",
+      description:
+        "This ia a project i did with html/css during my early days in web development..",
+      link: "https://sidebar-transitioned.vercel.app/",
     },
   ];
 
   return (
     <section id="projects" className="projects">
       <h2>Projects</h2>
-      <div className='project-box'>
-       <div>
-      <ul className='mainlinks'>
-        {projects.map((project, index) => (
-          <li key={index} className='mainlinks2'>
-            <h3>{project.name}</h3>
-            <p>{project.description}</p>
-            <a href={project.link} target="_blank" rel="noopener noreferrer">View Project</a>
-          </li>
-        ))}
-      </ul>
-      </div>
-      <img src={Image6} alt="Profile" className="project-pic" />
+      <div className="project-box">
+        <div>
+          <ul className="mainlinks">
+            {projects.map((project, index) => (
+              <li key={index} className="mainlinks2">
+                <h3>{project.name}</h3>
+                <p>{project.description}</p>
+                <a
+                  href={project.link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  View Project
+                </a>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <img src={Image6} alt="Profile" className="project-pic" />
       </div>
     </section>
   );
