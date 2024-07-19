17d6296af1f656d142e9b0292b9dffecb27adfb0
wezzbrown <sundaywisdomifiok@gmail.com>
2024-07-19 16:29:22 +0100

    portolio

--- src/component/tools.jsx
+++ src/component/tools.jsx
@@ -20,7 +20,7 @@ const Tools = () => {
                 <h2>Hello!</h2>
                     My name is Sunday Wisdom and I specialize in web developement that utilizes HTML, CSS, JS, and REACT etc.
                     I am a highly motivated individual and eternal optimist dedicated to writing clear, concise, robust code that works. Striving to never stop learning and improving.
-                    When I'm not coding, I am writing bolgs, reading, or picking up some new hands-on art project like photography.
+                    When I'm not coding, I am writing songs, reading, or picking up some new hands-on designs like fylers etc.
                     I like to have my perspective and belief systems challenged so that I see the world through new eyes.
                 </p>
           </div>
